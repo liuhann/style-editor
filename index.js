3172edf458aeb6ac0b85b4605151ec5e81cd0094
liuhan <liuhann@gmail.com>
2019-04-04 13:37:42 +0800

    import added

---
+++ index.js
@@ -0,0 +1,11 @@
+import PropConfig from './src/components/PropConfig.vue'
+import EditTransform from './src/components/EditTransform.vue'
+import EditClipPath from './src/components/EditClipPath.vue'
+
+export default PropConfig
+
+export {
+  PropConfig,
+  EditTransform,
+  EditClipPath
+}
